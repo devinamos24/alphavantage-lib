cbd8b0c5c9621bb10cfbf9280f50b9c98ce6efff
devinamos24 <devin.amos24@gmail.com>
2024-05-01 23:03:14 -0500

    Add Double.toStringWithoutScientific

--- src/common/src/com/jinxservers/alphavantage/util/Extension.kt
+++ src/common/src/com/jinxservers/alphavantage/util/Extension.kt
@@ -3,6 +3,7 @@ package com.jinxservers.alphavantage.util
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
 import kotlinx.datetime.minus
+import kotlin.math.abs
 
 internal fun <K,V> Pair<K,V>.toEntry() = object: Map.Entry<K,V> {
     override val key: K = first
@@ -13,3 +14,37 @@ internal fun String.substringBetween(startDelimiter: String, endDelimiter: Strin
     substringBefore(startDelimiter).substringAfter(endDelimiter)
 
 internal fun Instant.toEasternTime(): String = minus(5, DateTimeUnit.HOUR).toString().replace("Z", "-05:00")
+
+internal fun Double.toStringWithoutScientific(): String {
+    val string = this.toString()
+    if (string.contains("E")) {
+        val power = string.substringAfter("E").toInt()
+        val strippedString = string.substringBefore("E").replace(".", "").toMutableList()
+        if (power > 0) {
+            if (strippedString.size == power) {
+                strippedString.add('.')
+                strippedString.add('0')
+                return strippedString.joinToString("")
+            } else if (strippedString.size < power) {
+                repeat(strippedString.size - power + 1) {
+                    strippedString.add('0')
+                }
+                strippedString.add('.')
+                strippedString.add('0')
+                return strippedString.joinToString("")
+            } else {
+                strippedString.add(power + 1, '.')
+                return strippedString.joinToString("")
+            }
+        } else {
+            repeat(abs(power) - 1) {
+                strippedString.add(0, '0')
+            }
+            strippedString.add(0, '.')
+            strippedString.add(0, '0')
+            return strippedString.joinToString("")
+        }
+    } else {
+        return string
+    }
+}
